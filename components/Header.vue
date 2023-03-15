5114e440ebbdbffa8a8189d97bfb3335b2094448
BenoitEirik <olafsergent@gmail.com>
2023-03-15 21:28:44 +0100

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>Contemplor</header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: sticky;
+  margin-inline: 1rem;
+  padding-block: 1rem;
+  border-bottom: 1px solid $secondary;
+}
+</style>
