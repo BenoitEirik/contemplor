d57e2c5ca437bb15a87633982eee6301343e0dfb
BenoitEirik <olafsergent@gmail.com>
2023-03-15 21:18:38 +0100

    Set main parameters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: false,
+  css: ["@/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/_variables.scss";',
+        },
+      },
+    },
+  },
+  app: {
+    head: {
+      title: 'Contemplor'
+    }
+  }
 })
